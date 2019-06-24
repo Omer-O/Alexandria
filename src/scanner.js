a8efcbe53ef7f6f4d5348be0e44853ad697dd78f
Ben <bengold@protonmail.com>
2019-06-24 14:05:17 +0200

    ocr works

--- src/scanner.js
+++ src/scanner.js
@@ -1,13 +1,27 @@
 import React from "react";
 import axios from "./axios";
 import { Link } from "react-router-dom";
+import { TesseractWorker } from "tesseract.js";
+const worker = new TesseractWorker();
 
 export class Scanner extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedFile: null
-        };
+        this.state = {};
+    }
+    componentDidMount() {
+        const myImage = "./img/demo.jpg";
+        worker
+            .recognize(myImage)
+            .progress(progress => {
+                console.log("progress", progress);
+            })
+            .then(result => {
+                console.log("result", result);
+                this.setState({
+                    text: result.text
+                });
+            });
     }
     handleChange(event) {
         this.setState({});
@@ -19,11 +33,10 @@ export class Scanner extends React.Component {
                 <div className="x-btn" onClick={this.props.hideUploader}>
                     X
                 </div>
-                <div className="camera-display">
-                    ..............................
-                    ..............................
-                    ..............................
+                <div className="demo-img">
+                    <img src="./img/demo.jpg" />
                 </div>
+                <div className="display-text">{this.state.text}</div>
             </div>
         );
     }
